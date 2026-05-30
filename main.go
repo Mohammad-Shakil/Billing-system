22ff89dc558c335febc8713c9ebce7b0ac102c08
Gitshakil <md.fahim.sakil@gmail.com>
2026-05-30 12:20:12 +0600

    Add shop billing system functions

--- main.go
+++ main.go
@@ -2,33 +2,73 @@ package main
 
 import "fmt"
 
+func showWelcome() {
+	fmt.Println("Welcome to Shakil Shop")
+	fmt.Println("----------------------")
+}
+
+func showGoodbye() {
+	fmt.Println("----------------------")
+	fmt.Println("Thank you for shopping")
+	fmt.Println("That's all.....")
+}
+
+func showPrice(itemPrice int) {
+	fmt.Println("Price:", itemPrice)
+}
+
+func showQuantity(itemQuantity int) {
+	fmt.Println("Quantity:", itemQuantity)
+}
+
+func showTotal(total int) {
+	fmt.Println("Total bill:", total)
+}
+
+func showDiscount(discount int) {
+	fmt.Println("Discount:", discount)
+}
+func finalBeforeDelivery(finalBeforeDelivery int) {
+	fmt.Println("Final Before Delivery", finalBeforeDelivery)
+}
+func deliveryChargee(deliveryCharge int) {
+	fmt.Println("delivery charge:", deliveryCharge)
+}
+func payableAmountt(payableAmount int) {
+	fmt.Println("payable amount:", payableAmount)
+}
+
 func main() {
-	fmt.Println("Welcome to the billing system....")
-	fmt.Println()
+	showWelcome()
+
 	price := 10
 	quantity := 10
+
 	total := 0
 	total = price * quantity
+
 	discount := 0
 	if total >= 100 {
 		discount = 10
 	}
 
-	finalBeforedelivery := total - discount
+	finalBeforeDelivery := total - discount
 
 	deliveryCharge := 0
-	if finalBeforedelivery >= 100 {
+	if finalBeforeDelivery >= 100 {
 		deliveryCharge = 0
 	} else {
 		deliveryCharge = 20
 	}
-	payableAmount := finalBeforedelivery + deliveryCharge
-	fmt.Println("Price:", price)
-	fmt.Println("Quantity:", quantity)
-	fmt.Println("Total bill:", total)
-	fmt.Println("Discount:", discount)
-	fmt.Println("final Before delivery:", finalBeforedelivery)
-	fmt.Println("Delivery charge:", deliveryCharge)
-	fmt.Println("Payable amount:", payableAmount)
-	fmt.Println("thats all.....")
+
+	payableAmount := finalBeforeDelivery + deliveryCharge
+
+	showPrice(price)
+	showQuantity(quantity)
+	showTotal(total)
+	showDiscount(discount)
+	deliveryChargee(deliveryCharge)
+	payableAmountt(payableAmount)
+
+	showGoodbye()
 }
