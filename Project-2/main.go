fdcd6344fe95a75abe128108fd6ee68113a76dfd
Mohammad_Shakil <md.fahim.sakil@gmail.com>
2026-06-03 17:06:51 +0600

    learned struct and combined it with slice than called using a custom function

--- Project-2/main.go
+++ Project-2/main.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 
-type Book struct {
-	Title  string
-	Author string
-	Price  int
+type Toy struct {
+	Name     string
+	Price    int
+	Avilable bool
 }
 
+func isExpensive(toys Toy) bool {
+	return toys.Price >= 500
+}
 func main() {
-	books := []Book{
-		{Title: "(Go basics)", Author: "King Shakil", Price: 9999},
-		{Title: "(Rich dad Poor dad)", Author: "Joseph", Price: 7},
-		{Title: "(Human)", Author: "Robiul", Price: 10},
+
+	toys := []Toy{
+		{Name: "Arrma", Price: 400, Avilable: true},
+		{Name: "Dhik", Price: 200, Avilable: false},
+		{Name: "Wltoys", Price: 600, Avilable: true},
 	}
-	for i := 0; i < len(books); i++ {
-		fmt.Println(books[i].Title, books[i].Author, books[i].Price)
+	for i := 0; i < len(toys); i++ {
+		if isExpensive(toys[i]) {
+			fmt.Println("Toys is expensive", toys[i].Name)
+		}
 	}
 }
